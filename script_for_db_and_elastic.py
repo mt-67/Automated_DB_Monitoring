44ef545ff8aacf26c0c42c37a2ccd7bedb973459
Matvey <matvey@Galaxy-J7.localdomain>
2025-01-08 11:33:37 +0100

    add RAM usage by the database

---
+++ script_for_db_and_elastic.py
@@ -0,0 +1,17 @@
+import psycopg2 # connecting to the database
+import psutil  # CPU usage
+import time
+import requests  # sending data to elasticsearch
+
+
+'''RAM usage by the database'''
+def memory_usage():  
+
+    conn = psycopg2.connect(dbname="Aero", user="user_name",
+                            password="password", host="localhost")
+    cursor = conn.cursor()
+    cursor.execute("SELECT pg_total_relation_size(company, pass_in_trip, passenger, trip);")  
+    
+    memory_use = cursor.fetchone()[0]
+    conn.close()
+    return memory_use
